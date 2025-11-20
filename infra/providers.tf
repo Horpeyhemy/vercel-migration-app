6fd488399651602ba62dae3fc4f7f6263c721c49
Opeyemi <opeyemiatanda23@yahoo.com>
2025-11-20 02:49:25 -0800

    applyiness yesss ooo wow yaaay n

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,13 +1,13 @@
-terraform {
-  required_version = ">= 1.5.0"
-
+terraform { 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~> 3.70"
     }
   }
-} 
+
+  required_version = ">= 1.3.0"
+}
 
 provider "azurerm" {
   features {}
